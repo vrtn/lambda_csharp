0f4b9a22ee72c5ceb410d91f12e7920e9e03d9af
Zanini <everton.zanini@yahoo.com>
2022-05-19 17:28:43 -0300

    Envio do projeto didático

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lambda;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Tipo de função que não tem retorno.
+        Action escreverConsole = () =>
+        {
+            System.Console.WriteLine("Eu sou uma Action, não tenho retorno!");
+        };
+
+        escreverConsole();
+
+        // Tipo de função que apresenta retorno.
+        Func<int> Sorteiro = () =>
+        {
+            Random random = new Random();
+            return random.Next(1,101);
+        };
+
+        System.Console.WriteLine($"Eu sou uma Func, tenho um retono: {Sorteiro()}");
+
+    }
+}
